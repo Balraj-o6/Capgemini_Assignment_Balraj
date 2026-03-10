5d90e62f374f82cef4dafe6e37f0ea103fa44405
Balraj <balrajkv666@gmail.com>
2026-03-10 09:34:36 +0530

    Add files via upload

---
+++ 09-03-26 CriteriaAPIAssignment/src/main/java/com/cg/Employee.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Employee {
+    @Id
+    private int eid;
+    private String ename;
+    private String dept;
+}
